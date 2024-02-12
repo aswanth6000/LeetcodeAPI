184593dae541abb30d49e327ca14f3942277ef69
aswanth6000 <aswanth682@gmail.com>
2024-02-12 21:49:00 +0530

    home done

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image'
 import { Navbar } from '../components/Navbar'
 import { Poppins } from 'next/font/google'
+import  {ParticlesComponent} from '@/components/particles'
 const poppins = Poppins({
-  weight: '500',
+  weight: '700',
   subsets: ['latin'],
 })
 export default function Home() {
   return (
     <main className={`h-screen overflow-hidden w-full bg-black ${poppins.className}`} >
       <Navbar/>
-      <div className='h-screen w-full flex justify-center align-middle items-center'>
+     <ParticlesComponent/>
+      <div className='h-screen w-full flex justify-center align-middle mt-56'>
       <p className='text-white text-4xl'>
       your personal dashboard for tracking coding progress.
       </p>
---
+++ client/components/particles.tsx
@@ -0,0 +1,109 @@
+"use client"
+import { useCallback, useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import type { Container, Engine } from "@tsparticles/engine";
+// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+
+export const  ParticlesComponent = () => {
+    const [ init, setInit ] = useState(false);
+
+    // this should be run only once per application lifetime
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            //await loadAll(engine);
+            //await loadFull(engine);
+            await loadSlim(engine);
+            //await loadBasic(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+   const  particlesLoaded = (container: any) => {
+        console.log(container);
+    };
+
+    return (
+          <Particles className="w-full h-full absolute translate-z-0"
+            id="tsparticles"
+            particlesLoaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 2,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+
+)
+    ;
+};
