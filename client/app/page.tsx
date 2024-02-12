0e51fc148390d58f311963cd8eb3a4cd6a2b3bc8
aswanth6000 <aswanth682@gmail.com>
2024-02-12 11:44:06 +0530

    landing skelet done

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from 'next/image'
-
+import { Navbar } from '../components/Navbar'
+import { Poppins } from 'next/font/google'
+const poppins = Poppins({
+  weight: '500',
+  subsets: ['latin'],
+})
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main className={`h-screen overflow-hidden w-full bg-black ${poppins.className}`} >
+      <Navbar/>
+      <div className='h-screen w-full flex justify-center align-middle items-center'>
+      <p className='text-white text-4xl'>
+      your personal dashboard for tracking coding progress.
+      </p>
+      </div>
     </main>
   )
 }
